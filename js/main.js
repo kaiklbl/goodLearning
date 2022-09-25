e6f3fe45213ca509d76b558a72d22aa5201200b5
kaiklbl <kaikoelbl.developer@gmail.com>
2022-09-25 14:45:41 +0200

    esfunktJs

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ const page2 = $('#page2');
 
 
 const cardh3 = $('#card-h3');
-
+const question = $('#question');
+const answer = $('#answer');
+const homeBtn = $('#home-btn');
+const nextBtn = $('#next-btn');
 
 
 // erstes einblenden zweites ausblenden
@@ -17,8 +20,47 @@ const displayToFrom = (block,none) =>{
 }
 
 
+//--------------------------Page1------------------------------ 
+
 // Buttonfunktionen
 const toPage2 = name =>{
+    fetchData(name)
     displayToFrom(page2,page1);
     cardh3.textContent = name;
 }
+
+
+
+// ------------------------Page2------------------------------
+
+homeBtn.addEventListener('click',() =>{
+    displayToFrom(page1,page2);
+})
+
+
+
+// ---------------------------main Funktionen-----------------------
+
+const fetchData = name =>{
+    fetch(`data/${name}.json`)
+    .then(response => response.json())
+    .then(result => {
+        let random = randomNum(result.indexOf(result.at(-1))+1);     
+        question.textContent = result[random].question;
+        let num = 0;
+        nextBtn.addEventListener('click',e=>{
+            if(num == 0){
+                answer.textContent = result[random].answer;
+                num++;
+            }else{
+                answer.textContent = '';
+                random = randomNum(result.indexOf(result.at(-1))+1);
+                num--;
+                question.textContent = result[random].question;      
+            }
+        })
+    })
+    .catch(err => console.log(err));
+}
+
+const randomNum = result => Math.floor(Math.random()* result);
